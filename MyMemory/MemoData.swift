fbe55de43d545d6066df3de7dfcbd7ad5f2ed8a4
kchomg <kchomg@naver.com>
2021-09-15 20:09:50 +0900

    Initial Commit

---
+++ MyMemory/MemoData.swift
@@ -0,0 +1,16 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by Seok Eun Hong on 2021/09/13.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx: Int? // 데이터 식별값
+    var title: String? // 메모 제목
+    var contents: String? // 메모 내용
+    var image: UIImage? // 이미지
+    var regdate: Date? // 작성일
+}
---
+++ MyMemory/MemoListVC.swift
@@ -0,0 +1,54 @@
+//
+//  MemoListVC.swift
+//  MyMemory
+//
+//  Created by Seok Eun Hong on 2021/09/13.
+//
+
+import UIKit
+
+class MemoListVC: UITableViewController {
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let count = appDelegate.memolist.count
+        return count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 1. memolist 배열 데이터에서 주어진 행에 맞는 데이터를 꺼낸다.
+        let row = self.appDelegate.memolist[indexPath.row]
+        
+        // 2. 이미지 속성이 비어 있을 경우 "memoCell", 아니면 "memoCellWithImage"
+        let cellId = row.image == nil ? "memoCell" : "memoCellWithImage"
+        
+        // 3. 재사용 큐로부터 프로토타입 셀의 인스턴스를 전달받는다.
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as! MemoCell
+        
+        // 4. memoCell의 내용을 구성한다.
+        cell.subject?.text = row.title
+        cell.contents?.text = row.contents
+        cell.img?.image = row.image
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        cell.regdate?.text = formatter.string(from: row.regdate!)
+        
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let row = self.appDelegate.memolist[indexPath.row]
+        
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "MemoRead") as? MemoReadVC else {
+            return
+        }
+        
+        vc.param = row
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
