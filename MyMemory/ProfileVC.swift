3d97ed7fb068665e650e37c3f0b1e45e3368a872
kchomg <kchomg@naver.com>
2021-09-16 15:17:40 +0900

    개인 프로필 영역 추가

--- MyMemory/ProfileVC.swift
+++ MyMemory/ProfileVC.swift
@@ -16,6 +16,29 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         // 뒤로 가기 버튼 처리
         let backBtn = UIBarButtonItem(title: "닫기", style: .plain, target: self, action: #selector(close(_:)))
         self.navigationItem.leftBarButtonItem = backBtn
+        
+        // 프로필 사진에 들어갈 기본 이미지
+        let image = UIImage(named: "account.jpg")
+        
+        // 프로필 이미지 처리
+        self.profileImage.image = image
+        self.profileImage.frame.size = CGSize(width: 100, height: 100)
+        self.profileImage.center = CGPoint(x: self.view.frame.width / 2, y: 200)
+        
+        // 프로필 이미지 둥글게 만들기
+        self.profileImage.layer.cornerRadius = self.profileImage.frame.width / 2
+        self.profileImage.layer.borderWidth = 0
+        self.profileImage.layer.masksToBounds = true
+        
+        // 루트 뷰에 추가
+        self.view.addSubview(self.profileImage)
+        
+        // 테이블 뷰
+        self.tv.frame = CGRect(x: 0, y: self.profileImage.frame.origin.y + self.profileImage.frame.size.height + 20, width: self.view.frame.width, height: 100)
+        self.tv.delegate = self
+        self.tv.dataSource = self
+        
+        self.view.addSubview(self.tv)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -23,7 +46,22 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "cell")
+        
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 13)
+        cell.accessoryType = .disclosureIndicator
+        
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "이름"
+            cell.detailTextLabel?.text = "꼼꼼한 재은 씨"
+        case 1:
+            cell.textLabel?.text = "계정"
+            cell.detailTextLabel?.text = "sqlpro@naver.com"
+        default:
+        ()
+        }
         return cell
     }
     
